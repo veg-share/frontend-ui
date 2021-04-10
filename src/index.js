1279fd548a51ca5c3826bdb00c1bba0e537bb1fb
Allison Dietz <dietz.allison@outlook.com>
2021-04-10 14:23:46 -0600

    Setup initial file architecture, update imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorkerRegistration from './workerRegistration';
 import './index.css';
-import App from './App';
+import App from './App/App';
 
 ReactDOM.render(
     <App />,
