c0b15c890379ac173a27213e09f796cf57e08d19
colefiscus <>
2021-04-12 14:55:07 -0600

    Add render for App and imports in index.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,20 @@
-import logo from '../logo.svg';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import '../components/Header/Header';
+import '../components/Footer/Footer';
+import '../views/HomeView/HomeView';
+import '../views/ProfileView/ProfileView';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      
+      <Footer />
+    </>
+  )
 }
 
 export default App;
