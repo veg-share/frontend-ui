1bdf1c0ece3e976fc093af730d90f16bd8873f29
colefiscus <>
2021-04-12 15:04:28 -0600

    Add return to Header with Links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-
+  return (
+    <header>
+      <h1>Veg-Share</h1>
+      <Link className='profile-link' to='/profile'>Profile</Link>
+      <Link className='logout-link'>Logout</Link>
+    </header>
+  )
 }
 
 export default Header;
