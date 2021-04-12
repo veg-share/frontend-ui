64ee6bd8102bd726f564d97243ecfe018095c486
colefiscus <>
2021-04-12 15:49:47 -0600

    Add additional styling for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className='header'>
-      <h1>Veg-Share</h1>
+      <h1 className='veg-share-title'>Veg-Share</h1>
       <div className='header-links'>
         <Link className='profile-link' to='/profile'>Profile</Link>
         <Link className='logout-link'>Logout</Link>
