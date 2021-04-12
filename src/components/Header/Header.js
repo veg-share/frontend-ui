48879f74a8267922aacea83fd338e776dfdac0f5
colefiscus <>
2021-04-12 15:45:47 -0600

    Add basic styling for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
+    <header className='header'>
       <h1>Veg-Share</h1>
-      <Link className='profile-link' to='/profile'>Profile</Link>
-      <Link className='logout-link'>Logout</Link>
+      <div className='header-links'>
+        <Link className='profile-link' to='/profile'>Profile</Link>
+        <Link className='logout-link'>Logout</Link>
+      </div>
     </header>
   )
 }
