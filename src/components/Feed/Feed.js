eeddb3f87b41e8d2227ed8eaf5a21fc29cbaca80
Allison Dietz <dietz.allison@outlook.com>
2021-04-13 10:00:03 -0600

    Add Feed and Post imports/exports

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './Feed.css';
+
+// import Post from '../../components/Post/Post';
+
+const Feed = () => {
+
+}
+
+export default Feed;
---
+++ src/components/Post/Post.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './Post.css';
+
+const Post = () => {
+
+}
+
+export default Post;
