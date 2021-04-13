ab2b6687f098b572165cc2b1f5e94d895840de0d
Allison Dietz <dietz.allison@outlook.com>
2021-04-12 19:01:32 -0600

    Add imports/export to begin HomeView component

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './HomeView.css';
+// import Header from '../../components/Header/Header';
+// import Footer from '../../components/Footer/Footer';
+// import UserInteractions from '../../components/UserInteractions/UserInteractions';
+// import FormModal from '../../components/FormModal/FormModal';
+// import Search from '../../components/Search/Search';
+// import Feed from '../../components/Feed/Feed';
+// import Post from '../../components/Post/Post';
+
+
+export default HomeView;
