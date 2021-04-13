3f67571770f2bc95399f01e3c98630732ac43889
Allison Dietz <dietz.allison@outlook.com>
2021-04-13 09:58:06 -0600

    Add Search imports/export

---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './Search.css';
+
+const Search = () => {
+
+}
+
+export default Search;
