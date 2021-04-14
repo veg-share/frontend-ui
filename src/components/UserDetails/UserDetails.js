6bbb69f77300e4f20d4aebdbb81862f3a2d5da33
Christina Cook <cmc.cook7@gmail.com>
2021-04-13 19:08:16 -0600

    Add icons and style user details

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './UserDetails.css';
+import profilePic from '../../Images/user.png';
+import homeIcon from '../../Images/home.png';
+import veggieIcon from '../../Images/carrot.png';
 
 const UserDetails = ({user}) => {
   return (
     <div className='user-container'>
-      {/*<img src= alt='profile-picture' className='profile-picture'/> */}
-      <h3 className='user-name'>{user.name}</h3>
-      <p className='user-bio'>{user.bio}</p>
-      <p className='user-location'>{user.location}</p>
-      <p className='user-shares'>Veg Shares: {user.numberOfShares}</p>
+      <img src={profilePic} alt='profile-picture' className='profile-pic'/>
+      <div className='user-details'>
+        <div className='location-container'>
+          <img src={homeIcon} alt='house' className='home-img'/>
+          <p className='user-location'>{user.location}</p>
+        </div>
+        <div className='shares-container'>
+          <img src={veggieIcon} alt='carrot' className='veggie-img'/>
+          <p className='user-shares'>Veg Shares: {user.numberOfShares}</p>
+        </div>
+        <p className='user-bio'>{user.bio}</p>
+      </div>
     </div>
   )
 }
