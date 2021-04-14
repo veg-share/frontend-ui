3b13bf41b5f7424d76c41daab7450eb2a0cd348f
Christina Cook <cmc.cook7@gmail.com>
2021-04-14 10:36:07 -0600

    Display posts in profile feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -3,12 +3,40 @@ import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import './Feed.css';
-
-// import Post from '../../components/Post/Post';
+import Post from '../../components/Post/Post';
+import { user } from '../../Data/mockData';
 
 const Feed = () => {
+  const [currentUserPosts, setCurrentUserPosts] = useState([])
+
+  useEffect(() => {
+    console.log('user.posts', user.posts)
+    const userPosts = user.posts
+    console.log('user posts', userPosts)
+    setCurrentUserPosts(userPosts)
+    console.log('currentUserPosts', currentUserPosts)
+  }, [currentUserPosts])
+
+  const displayPostsForCurrentUser = (postsToDisplay) => {
+    return postsToDisplay.map(post => {
+      return (
+        <Post
+          userName={post.userName}
+          title={post.title}
+          date={post.date}
+          location={post.location}
+          description={post.description}
+          distance={post.distance}
+          imageUrl={post.image}
+        />
+      )
+    })
+  }
+
   return (
-    <h2>Feed goes here</h2>
+    <div className='post-container'>
+      {displayPostsForCurrentUser(currentUserPosts)}
+    </div>
   )
 }
 
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import profilePic from '../../Images/user.png';
 import './Post.css';
 
-const Post = () => {
-
+const Post = ({ userName, title, date, location, description, distance, imageUrl }) => {
+  return (
+    <div className='single-post'>
+      <div className='post-header'>
+        <img src='profilePic' alt='profile-picture' className='post-profile-pic'/>
+        <p>{userName}</p>
+        <p>{date}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Post;
