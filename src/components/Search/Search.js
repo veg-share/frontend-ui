4a4420f942a98a4550d03281af2aaf60eb6ae9fd
Christina Cook <cmc.cook7@gmail.com>
2021-04-15 15:23:10 -0600

    Add search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,7 +5,12 @@ import PropTypes from 'prop-types';
 import './Search.css';
 
 const Search = () => {
-
+  return (
+    <div className='search'>
+      <input type='text' className='search-bar' placeholder='Search'/>
+      <button type='submit' className='search-button'></button>
+    </div>
+  )
 }
 
 export default Search;
--- src/components/UserInteractions/UserInteractions.js
+++ src/components/UserInteractions/UserInteractions.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './UserInteractions.css';
-// import Search from '../Search/Search';
+import Search from '../Search/Search';
 // import FormModal from '../FormModal/FormModal';
 
 const UserInteractions = () => {
   return (
     <div className='interactions-container'>
       {/* conditionally render search component based on view */}
+      <Search />
       <button className='post-button'>Create Post</button>
     </div>
   )
