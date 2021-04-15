b167b3884cca0cb07cedbea2c68d2d5733b69412
colefiscus <>
2021-04-15 13:37:56 -0600

    Add new files w/ diff names and edit index import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import * as serviceWorkerRegistration from './workerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 import App from './App/App';
 
