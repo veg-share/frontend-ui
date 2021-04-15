e3628669d5ad0abbdeee9a651f916434aa02194e
Allison Dietz <dietz.allison@outlook.com>
2021-04-14 20:24:13 -0600

    Update cypress tests to use beforeEach, pass starter tests

--- cypress/integration/homeview_spec.js
+++ cypress/integration/homeview_spec.js
@@ -1,15 +1,8 @@
 describe('VegShare, main page view', () => {
   const baseUrl = 'https://veg-share.herokuapp.com'
 
-  beforeAll(() => {
-  //   cy
-  //   .fixture('mockData.json')
-  //   .then((mockData) => {
-  //     cy.intercept('GET', )
-  //   })
-
+  beforeEach(() => {
     cy.visit(baseUrl)
-
   })
 
   it ('should have the correct url for the home page on load', () => {
@@ -19,7 +12,7 @@ describe('VegShare, main page view', () => {
 
   it ('should show the site header', () => {
     cy
-      .get('header .veg-share-title').contains('Veg-Share')
-  })
+      .get('header .veg-share-title').should('contain', 'Veg-Share')
+  });
 
 })
