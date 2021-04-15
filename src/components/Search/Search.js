497e84689c2d36720a76695199fc37279372a5b8
Christina Cook <cmc.cook7@gmail.com>
2021-04-15 15:30:33 -0600

    Remove unnecessary imports

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Search.css';
