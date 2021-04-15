ce15c585d6cda608ba9d44c8db1223365778a426
colefiscus <>
2021-04-15 15:13:03 -0600

    Edit Cypress test

--- cypress/integration/homeview_spec.js
+++ cypress/integration/homeview_spec.js
@@ -1,25 +1,14 @@
 describe('VegShare, main page view', () => {
   const baseUrl = 'https://veg-share.herokuapp.com'
-
-  beforeAll(() => {
-  //   cy
-  //   .fixture('mockData.json')
-  //   .then((mockData) => {
-  //     cy.intercept('GET', )
-  //   })
-
+  beforeEach(() => {
     cy.visit(baseUrl)
-
   })
-
   it ('should have the correct url for the home page on load', () => {
     cy
       .url().should('eq', `${baseUrl}/`)
   });
-
   it ('should show the site header', () => {
     cy
-      .get('header .veg-share-title').contains('Veg-Share')
-  })
-
+      .get('header .veg-share-title').should('contain', 'Veg-Share')
+  });
 })
