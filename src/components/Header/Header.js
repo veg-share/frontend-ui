d80c43e0a06dea0e21d2d617e063ce0b33b1646c
colefiscus <>
2021-04-17 13:54:34 -0600

    Update header for CI test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className='header'>
-      <h1 className='veg-share-title'>Garden Party</h1>
+      <h1 className='veg-share-title'>Garden Party 2</h1>
       <div className='header-links'>
         <Link className='home-link' to='/'>Home</Link>
         <Link className='profile-link' to='/profile'>Profile</Link>
