ddc227622081c996876b64cdc3b44dd3c23417aa
colefiscus <>
2021-04-17 14:08:58 -0600

    Change Cypress test baseURL

--- cypress/integration/homeview_spec.js
+++ cypress/integration/homeview_spec.js
@@ -2,10 +2,6 @@ describe('VegShare, main page view', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it ('should have the correct url for the home page on load', () => {
-    cy
-      .url().should('eq', `http://localhost:3000`)
-  });
   it ('should show the site header', () => {
     cy
       .get('header .veg-share-title').should('contain', 'Garden Party')
