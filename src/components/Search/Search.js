d3532b857d101e940ef10a840b66eb8f47a7acba
Christina Cook <cmc.cook7@gmail.com>
2021-04-17 12:53:06 -0600

    Add notes on search functionality

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,14 +2,29 @@ import React, { useState } from 'react';
 import './Search.css';
 
 const Search = () => {
+  // props needed for search funtionality: allPosts
+  // might try using context instead of having to pass these all the way down
+  // we'll also need access to a method on the feed component that determines which posts to display based on the search term
+
   const [searchInput, setSearchInput] = useState('')
 
   const handleSearch = (event) => {
     setSearchInput(event.target.value)
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    filterPosts()
+  }
+
+  const filterPosts = () => {
+    // filter over the array of posts
+    // return all posts that include searchInput in the title or description
+    // call a method to determine which posts to display (I think this method would live in the feed component)
+  }
+
   return (
-    <form className='search'>
+    <form className='search' onSubmit={handleSubmit}>
       <input
         type='text'
         className='search-bar'
