737b5ab119370d7dbbf4b612c653ed5c501a240f
colefiscus <>
2021-04-17 17:17:58 -0600

    Add profile message indicating presence of posts

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -11,24 +11,32 @@ const Feed = ({ profileIsVisible }) => {
   const [allPosts, setAllPosts] = useState([])
 
   useEffect(() => {
-    setCurrentUserPosts(user.posts)
+    // setCurrentUserPosts(user.posts)
     setAllPosts(posts)
   }, [currentUserPosts, allPosts])
 
   const displayPostsInFeed = (postsToDisplay) => {
-    return postsToDisplay.map(post => {
+    if (postsToDisplay.length) {
+      return postsToDisplay.map(post => {
+        return (
+          <Post
+            userName={post.userName}
+            title={post.title}
+            date={post.date}
+            location={post.location}
+            description={post.description}
+            distance={post.distance}
+            imageUrl={post.image}
+          />
+        )
+      })
+    } else {
       return (
-        <Post
-          userName={post.userName}
-          title={post.title}
-          date={post.date}
-          location={post.location}
-          description={post.description}
-          distance={post.distance}
-          imageUrl={post.image}
-        />
+        <section className='no-user-posts-message'>
+          <p>Looks like you haven't made any shares yet. Click the 'Create Post' button above to join the party!</p>
+        </section>
       )
-    })
+    }
   }
 
   return (
