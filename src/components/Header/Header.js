77128e7b1b153a5197dfdae2cc937ae4ff411b21
colefiscus <>
2021-04-17 17:53:29 -0600

    Update method to properly iterate on nodelist

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,22 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 
   const toggleLink = () => {
-    const homeLink = document.querySelector('.home-link')
-    const profileLink = document.querySelector('.profile-link')
-    homeLink.classList.toggle('hidden')
-    profileLink.classList.toggle('hidden')
+    const homeLink = document.querySelectorAll('.home-link')
+    const profileLink = document.querySelectorAll('.profile-link')
+    homeLink.forEach(e => e.classList.toggle('hidden'))
+    profileLink.forEach(e => e.classList.toggle('hidden'))
   }
 
   return (
     <header className='header'>
       <h1 className='veg-share-title'>Garden Party</h1>
       <div className='header-links'>
-        <Link className='home-link hidden' to='/' onClick={() => toggleLink()}>To Home</Link>
-        <Link className='profile-link' to='/profile' onClick={() => toggleLink()}>To Profile</Link>
+        <button className='home-link hidden'>
+          <Link className='home-link hidden' to='/' onClick={() => toggleLink()}>To Home</Link>
+        </button>
+        <button className='profile-link'>
+          <Link className='profile-link' to='/profile' onClick={() => toggleLink()}>To Profile</Link>
+        </button>
       </div>
     </header>
   )
