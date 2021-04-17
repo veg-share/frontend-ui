3d527443ccaa21735736ef13271ee89f780d092b
Christina Cook <cmc.cook7@gmail.com>
2021-04-17 12:30:52 -0600

    Set search input in state

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 
 const Search = () => {
+  const [searchInput, setSearchInput] = useState('')
+
+  const handleSearch = (event) => {
+    setSearchInput(event.target.value)
+  }
+
   return (
-    <div className='search'>
-      <input type='text' className='search-bar' placeholder='Search'/>
-      <button type='submit' className='search-button'>
+    <form className='search'>
+      <input
+        type='text'
+        className='search-bar'
+        placeholder='Search by produce name'
+        onChange={handleSearch}
+      />
+      <button
+        type='submit'
+        className='search-button'>
         <i className="fa fa-search"></i>
       </button>
-    </div>
+    </form>
   )
 }
 
