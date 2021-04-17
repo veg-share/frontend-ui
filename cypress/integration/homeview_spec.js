3a9af91f4d0390bc2d9d6e7c4031762da82ca3d8
colefiscus <>
2021-04-17 11:58:31 -0600

    Change cypress url test

--- cypress/integration/homeview_spec.js
+++ cypress/integration/homeview_spec.js
@@ -1,11 +1,10 @@
 describe('VegShare, main page view', () => {
-  const baseUrl = 'https://veg-share.herokuapp.com'
   beforeEach(() => {
-    cy.visit(baseUrl)
+    cy.visit('/')
   })
   it ('should have the correct url for the home page on load', () => {
     cy
-      .url().should('eq', `${baseUrl}/`)
+      .url().should('eq', `https://frontend-ui.vercel.app/`)
   });
   it ('should show the site header', () => {
     cy
