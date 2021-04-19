c9317e6e9ee90f23641b89638f622872436ec772
Christina Cook <cmc.cook7@gmail.com>
2021-04-19 11:49:31 -0600

    Set context for all posts

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import HomeView from '../views/HomeView/HomeView';
 import ProfileView from '../views/ProfileView/ProfileView';
+import AppContext from '../Context/AppContext';
+import { posts } from '../Data/mockData';
 
 const App = () => {
+  const [allPosts, setAllPosts] = useState([])
+
+  useEffect(() => {
+    setAllPosts(posts)
+  }, [allPosts])
 
   return (
     <>
-      <Header />
-      <Route exact path='/' component={HomeView} />
-      <Route exact path='/profile' component={ProfileView} />
-      <Footer />
+      <AppContext.Provider value={allPosts}>
+        <Header />
+        <Route exact path='/' component={HomeView} />
+        <Route exact path='/profile' component={ProfileView} />
+        <Footer />
+      </AppContext.Provider>
     </>
   )
 }
