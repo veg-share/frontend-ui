67e481e7f59ccdf8e69b0dcd3580d9ab0575c0ba
Christina Cook <cmc.cook7@gmail.com>
2021-04-19 14:08:44 -0600

    Remove comments

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -10,47 +10,13 @@ import Feed from '../../components/Feed/Feed';
 const HomeView = () => {
   const [isHome, setIsHome] = useState(false)
   const [searchResults, setSearchResults] = useState([])
-  // const [allPosts, setAllPosts] = useState([]);
-  // const [userLocation, setUserLocation] = useState({});
-  // const [error, setError] = useState('');
-
-  // const getUserLocation = async () => {
-  //   // update with URL for geocoding location API
-  //   const url = '';
-  //   setError('');
-  //
-  //   try {
-  //     const response = await fetch(url)
-  //     const userLocation = await response.json()
-  //     setUserLocation(userLocation);
-  //   } catch(error) {
-  //     setError(error.message);
-  //   }
-  // }
-  //
-  // const getAllPosts = async () => {
-  //   // update with URL to BE to request from when avail.
-  //   const url = '';
-  //   setError('');
-  //
-  //   try {
-  //     const response = await fetch(url)
-  //     const allPosts = await response.json()
-  //     setAllPosts(allPosts);
-  //   } catch(error) {
-  //     setError(error.message);
-  //   }
-  // }
 
   useEffect(() => {
     setIsHome(true)
-    // getUserLocation();
-    // getAllPosts();
-    }, [])
+  }, [])
 
   const determineSearchResults = (posts) => {
     setSearchResults(posts)
-    console.log('searchResults', searchResults)
   }
 
   return (
