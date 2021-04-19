bc1d9ac724c590970e9c4cbe3b729b61a7f1d694
Christina Cook <cmc.cook7@gmail.com>
2021-04-19 14:00:59 -0600

    Remove comments

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,13 +3,8 @@ import './Search.css';
 import AppContext from '../../Context/AppContext';
 
 const Search = ({ determineSearchResults }) => {
-  // props needed for search funtionality: allPosts
-  // might try using context instead of having to pass these all the way down
-  // we'll also need access to a method on the feed component that determines which posts to display based on the search term
-
   const [searchInput, setSearchInput] = useState('')
   const allPosts = useContext(AppContext)
-
 
   const handleSearch = (event) => {
     setSearchInput(event.target.value)
@@ -21,13 +16,9 @@ const Search = ({ determineSearchResults }) => {
   }
 
   const filterPosts = () => {
-    // filter over the array of posts
-    // return all posts that include searchInput in the title or description
     const postsToDisplay = allPosts.filter(post => {
       return post.title.includes(searchInput) || post.description.includes(searchInput)
     })
-    console.log('postsToDisplay', postsToDisplay)
-    // call a method to determine which posts to display (I think this method would live in the feed component)
     determineSearchResults(postsToDisplay)
   }
 
