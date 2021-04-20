71e9c4a448ac2819019a3c8911ca99b4e16e5841
Allison Dietz <dietz.allison@outlook.com>
2021-04-20 15:39:53 -0600

    Call graphQL query in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
 import App from './App/App';
 
 import './index.css';
 
 const client = new ApolloClient({
-  uri: 'https://garden-party-veg.herokuapp.com/api/v1/users',
+  uri: 'https://garden-party-veg.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
+
+client
+  .query({
+    query: gql`
+      query getAllUsers {
+        username
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <Router>
