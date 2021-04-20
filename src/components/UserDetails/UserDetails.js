1974c787c29585b04fc4979bcb92e5e9d160d0a3
Allison Dietz <dietz.allison@outlook.com>
2021-04-20 01:18:35 -0600

    Updates to UserDetails styling, adjust fonts and icons

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -2,27 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import profilePic from '../../Images/user.png';
-import homeIcon from '../../Images/home.png';
-import veggieIcon from '../../Images/carrot.png';
+import homeIcon from '../../Images/home2.png';
+import veggieIcon from '../../Images/carrot2.png';
 import './UserDetails.css';
 
 const UserDetails = ({user}) => {
   return (
-    <div className='user-details-container'>
+    <section className='user-details-container'>
       <img src={profilePic} alt='user profile' className='profile-pic'/>
       <h2 className='user-name'>{user.name}</h2>
-      <div className='user-details'>
+      <section className='user-details'>
         <div className='location-container'>
           <img src={homeIcon} alt='house icon' className='home-img'/>
           <p className='user-location'>{user.location}</p>
         </div>
         <div className='shares-container'>
           <img src={veggieIcon} alt='carrot icon' className='veggie-img'/>
-          <p className='user-shares'>Veg Shares: {user.numberOfShares}</p>
+          <p className='user-shares'>{user.numberOfShares} veg shares!</p>
         </div>
         <p className='user-bio'>{user.bio}</p>
-      </div>
-    </div>
+      </section>
+    </section>
   )
 }
 
