b537f0545bf2a03fae1557db98df06d6af69fa0d
Allison Dietz <dietz.allison@outlook.com>
2021-04-20 14:58:17 -0600

    Install Apollo and GraphQL dependencies, create client in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import './index.css';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './App/App';
+
+import './index.css';
+
+const client = new ApolloClient({
+  uri: 'https://garden-party-veg.herokuapp.com/api/v1/users',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <Router>
